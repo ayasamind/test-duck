303405915ca7284810062b93162df7909c089545
ayasamind <masayaliketennis@yahoo.co.jp>
2018-02-28 14:12:57 +0900

    ヘッダーを設置

---
+++ app/javascript/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="main-header">
+    <!-- Logo -->
+    <router-link to='awsaccounts' class='logo'>
+      <span class="logo-lg">
+        Captain-Duck
+      </span>
+    </router-link>
+
+    <nav class="navbar navbar-static-top">
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+      <div class="navbar-custom-menu">
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
---
+++ app/javascript/components/Sidebar.vue
@@ -0,0 +1,25 @@
+<template>
+  <aside class="main-sidebar">
+    <section class="sidebar" style="height: auto;">
+      <ul class="sidebar-menu tree" data-widget="tree">
+        <li class="header">メニュー</li>
+        <li>
+          <router-link to='awsaccounts' class='logo'>
+            アカウント一覧
+          </router-link>
+        </li>
+        <li>
+          <router-link to='CreateAwsAccounts' class='logo'>
+            アカウント新規作成
+          </router-link>
+        </li>
+      </ul>
+    </section>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar',
+}
+</script>
