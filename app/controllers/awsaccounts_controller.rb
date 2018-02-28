7688c1128a4e022523083cdddb6cf1c5e8f5aed3
ayasamind <masayaliketennis@yahoo.co.jp>
2018-02-28 14:09:44 +0900

    awsaccountsのindex画面を作成

---
+++ app/controllers/awsaccounts_controller.rb
@@ -0,0 +1,21 @@
+class AwsaccountsController < ApplicationController
+  def index
+    @aws_accounts = AwsAccount.order("id");
+  end
+
+  def show
+
+  end
+
+  def new
+    @aws_account = AwsAccount.new
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+end
