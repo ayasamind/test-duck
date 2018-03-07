cd4c58a42d9b0cd3bfd6e829a4edd17b40ae886a
ayasamind <masayaliketennis@yahoo.co.jp>
2018-03-07 06:36:04 +0000

    アカウント詳細画面を追加

--- app/javascript/packs/routes.js
+++ app/javascript/packs/routes.js
@@ -2,9 +2,12 @@
 //routerの設定ファイル
 import AwsAccounts from 'components/AwsAccounts'
 import CreateAwsAccounts from 'components/CreateAwsAccounts'
+import ShowAwsAccount from 'components/ShowAwsAccount'
+
 const routes = [
   { path:'/', component: AwsAccounts },
-  { path:'/createawsaccounts', component: CreateAwsAccounts }
+  { path:'/createawsaccounts', component: CreateAwsAccounts },
+  { path:'/showawsaccount/:id', name:'ShowAwsAccount', component: ShowAwsAccount },
 ]
 
 export default routes
