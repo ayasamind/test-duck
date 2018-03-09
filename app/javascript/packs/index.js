1524487de837d2736baa6934daac5bc3be63a6a8
ayasamind <masayaliketennis@yahoo.co.jp>
2018-03-09 14:22:13 +0900

    ディレクトリ構成を変更

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from '../app.vue'
+import store from '../vuex/store'
 
-const app = new Vue({
-  render: h => h(App)
+//routerの設定
+import router from './router'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const el = document.body.appendChild(document.createElement('index'))
+    const app = new Vue({
+        store,
+        el,
+        router,
+        render: h => h(App)
+    })
 })
