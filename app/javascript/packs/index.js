a52ba8a58b14af8dd7c989db8ae02f631a535adf
ayasamind <masayaliketennis@yahoo.co.jp>
2018-03-14 13:37:23 +0900

    vue-dev-toolのバグ対策(settimeout)

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -7,10 +7,21 @@ import router from './router'
 
 document.addEventListener('DOMContentLoaded', () => {
     const el = document.body.appendChild(document.createElement('index'))
-    const app = new Vue({
-        store,
-        el,
-        router,
-        render: h => h(App)
-    })
+    if (process.env.NODE_ENV !== 'production') {
+        setTimeout(() => {
+            const app = new Vue({
+                store,
+                el,
+                router,
+                render: h => h(App)
+            })
+        }, 500);
+    } else {
+        const app = new Vue({
+            store,
+            el,
+            router,
+            render: h => h(App)
+        })
+    }
 })
