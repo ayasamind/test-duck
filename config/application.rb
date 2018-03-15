8c21388005517c10b7d8efd894a883b021da59d2
withelmo <withelmo@gmail.com>
2018-03-15 21:42:02 +0900

    Add setting and i18n for aws_account enums

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,26 @@ module CaptainDuck
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # lib配下をオートロードの対象とする
+    config.autoload_paths += Dir[Rails.root.join('lib')]
+    config.autoload_paths += Dir[Rails.root.join('lib', '**', '*')]
+
+    # development/productionどちらでもAutoloadを行う
+    config.enable_dependency_loading = true
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+
+    config.active_record.default_timezone = :local
+
+    # cron処理用のlogger
+    config.cron_logger = Logger.new('log/cron.log', 'weekly')
+
+    # Not create test files
+    config.generators do |g|
+      g.test_framework false
+    end
   end
 end
