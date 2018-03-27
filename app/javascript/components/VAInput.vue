735738c3d576d987f4d2adfde647d519c3374f10
ayasamind <masayaliketennis@yahoo.co.jp>
2018-03-27 17:33:32 +0900

    create画面を作成

---
+++ app/javascript/components/VAInput.vue
@@ -0,0 +1,85 @@
+<template>
+
+  <div v-if="isHorizontal"
+    class="form-group"
+  >
+
+    <label :for="vaId" class="col-sm-2 control-label">{{ title }}</label>
+    <div class="col-sm-10">
+      <input
+        :type="type"
+        :class="[formControl]"
+        :id="vaId"
+        :placeholder="placeholder"
+        :value="value"
+        @blur="sendParams($event.target.value)"
+        >
+      </input>
+    </div>
+
+  </div>
+  <input v-else
+         :type="type"
+         :class="[formControl]"
+         :id="vaId"
+         :placeholder="placeholder"
+         :value="value"
+         :disabled="isDisabled"
+         @blur="sendParams($event.target.value)"
+         >
+  </input>
+
+
+</template>
+
+<script>
+export default {
+  name: 'va-input',
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+    vaId: {
+      type: String,
+    },
+    placeholder: {
+      type: String,
+      default: 'Enter Text...',
+    },
+    size: {
+      type: String,
+    },
+    value: {
+      type: String,
+    },
+    isFormControl: {
+      type: Boolean,
+      default: true,
+    },
+    isHorizontal: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    sendParams(value) {
+      this.$emit('input', value);
+    },
+  },
+  computed: {
+    formControl() {
+      return this.isFormControl ? 'form-control' : '';
+    },
+  },
+  created() {
+  },
+};
+</script>
---
+++ app/javascript/components/VASelect.vue
@@ -0,0 +1,68 @@
+<template>
+  <div v-if="isHorizontal"
+    class="form-group"
+  >
+    <label :for="vaId" class="col-sm-2 control-label">{{ title }}</label>
+    <div class="col-sm-10">
+      <select
+        class="form-control"
+        :disabled="isDisabled"
+        :multiple="isMultiple"
+        @change="sendParams($event.target.value)"
+      >
+        <option v-if="isEmpty" value=""></option>
+        <option v-for="(item,index) in list" :value="index" :data="item" :key="index">
+          {{ item }}
+        </option>
+      </select>
+    </div>
+  </div>
+  <select v-else
+    class="form-control" :disabled="isDisabled" :multiple="isMultiple">
+      <option v-if="isEmpty" value=""></option>
+    <option v-for="(item,index) in list" :data="item" :key="index">
+      {{ item }}
+    </option>
+  </select>
+</template>
+<script>
+export default {
+  name: 'va-select',
+  props: {
+    list: {
+      type: Object,
+      default: {},
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+    isMultiple: {
+      type: Boolean,
+      default: false,
+    },
+    isHorizontal: {
+      type: Boolean,
+      default: false,
+    },
+    isEmpty: {
+      typeL: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+    },
+    vaId: {
+      type: String,
+    },
+  },
+  methods: {
+    sendParams(value) {
+      this.$emit('input', Number(value));
+    },
+  },
+  created() {
+
+  },
+};
+</script>
