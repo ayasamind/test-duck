3691d18f6272e8abe1a028fa6e8d9eccabc6fea2
ayasamind <masayaliketennis@yahoo.co.jp>
2018-03-27 17:35:52 +0900

    rails側のバリデーションを追加

--- app/models/aws_account.rb
+++ app/models/aws_account.rb
@@ -12,6 +12,15 @@ class AwsAccount < ApplicationRecord
 
   #----------------------------------------------------------------------------
   # validation
+  validates :account_id,  presence: true
+  validates :email, presence: true
+  validates :account_name, presence: true
+  validates :display_name, presence: true
+  validates :category, presence: true
+  validates :payer_account_id, presence: true
+  validates :user_id, presence: true
+  validates :purpose, presence: true
+  validates :status, presence: true
 
   #----------------------------------------------------------------------------
   # event
